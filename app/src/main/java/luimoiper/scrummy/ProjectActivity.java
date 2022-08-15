cbadfaf6c3002342f5b6fab2d0b6e078b305cb85
LuiMoiPer <luimoiper@outlook.com>
2022-08-14 21:32:30 -0700

    Added backlog and sprint buttons to ProjectActivity

--- app/src/main/java/luimoiper/scrummy/ProjectActivity.java
+++ app/src/main/java/luimoiper/scrummy/ProjectActivity.java
@@ -2,9 +2,17 @@ package luimoiper.scrummy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import luimoiper.scrummy.models.ProjectModel;
 
@@ -14,12 +22,17 @@ public class ProjectActivity extends AppCompatActivity {
     private TextView title;
     private TextView description;
 
+    private Button backlogButton;
+    private Button sprintsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.project_activity);
         title = findViewById(R.id.title);
         description = findViewById(R.id.description);
+        backlogButton = findViewById(R.id.backlogButton);
+        sprintsButton = findViewById(R.id.sprintsButton);
 
         Intent intent = getIntent();
         projectModel = intent.getParcelableExtra("ProjectModel");
@@ -31,10 +44,25 @@ public class ProjectActivity extends AppCompatActivity {
         if (projectModel != null) {
             title.setText(projectModel.getTitle());
             description.setText(projectModel.getDescription());
+        } else {
+            title.setText("Project model is NULL");
+            description.setText("Project model is NULL");
         }
-        else {
-            title.setText("NULL");
-            description.setText("NULL");
-        }
+    }
+
+    private void setButtonActions() {
+        backlogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        sprintsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
