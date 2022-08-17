2915478448565b6748acf8c09e07499bdf5a6614
LuiMoiPer <luimoiper@outlook.com>
2022-08-17 10:59:44 -0700

    Shows backlog items for a project using a fragment

--- app/src/main/java/luimoiper/scrummy/ui/ProjectActivity.java
+++ app/src/main/java/luimoiper/scrummy/ui/ProjectActivity.java
@@ -3,31 +3,43 @@ package luimoiper.scrummy.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
 
 import luimoiper.scrummy.R;
 import luimoiper.scrummy.models.ProjectModel;
+import luimoiper.scrummy.utils.Generator;
 
 public class ProjectActivity extends AppCompatActivity {
     private ProjectModel projectModel;
+    private FragmentManager fragmentManager;
+    private ListFragment backlogItemsFragment;
+    private ListFragment sprintsFragment;
 
     private TextView title;
     private TextView description;
 
+    private FragmentContainerView fragmentContainer;
     private Button backlogButton;
     private Button sprintsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fragmentManager = getSupportFragmentManager();
+        backlogItemsFragment = new ListFragment(
+                new TaskAdapter(Generator.makeTaskModels(20), this::onBacklogItemClick)
+        );
+
         setContentView(R.layout.project_activity);
         title = findViewById(R.id.title);
         description = findViewById(R.id.description);
+        fragmentContainer = findViewById(R.id.fragmentContainer);
         backlogButton = findViewById(R.id.backlogButton);
         sprintsButton = findViewById(R.id.sprintsButton);
 
@@ -38,6 +50,11 @@ public class ProjectActivity extends AppCompatActivity {
     }
 
     private void setViewContent() {
+        fragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(fragmentContainer.getId(), backlogItemsFragment)
+                .commit();
+
         if (projectModel != null) {
             title.setText(projectModel.getTitle());
             description.setText(projectModel.getDescription());
@@ -58,5 +75,13 @@ public class ProjectActivity extends AppCompatActivity {
         sprintsButton.setOnClickListener(view -> {
             Toast.makeText(context, "Sprint button", Toast.LENGTH_SHORT).show();
         });
+    }
+
+    private void onBacklogItemClick(int position) {
+        Toast.makeText(this, "Backlog item at position " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    private void onSprintItemClick(int position) {
+        // do stuff
     }
 }
---
+++ app/src/main/java/luimoiper/scrummy/ui/TaskAdapter.java
@@ -0,0 +1,70 @@
+package luimoiper.scrummy.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import luimoiper.scrummy.R;
+import luimoiper.scrummy.models.TaskModel;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
+    private TaskModel[] taskModels;
+    private ListItemListener listItemListener;
+
+    public TaskAdapter(TaskModel[] taskModels, ListItemListener listItemListener) {
+        this.taskModels = taskModels;
+        this.listItemListener = listItemListener;
+    }
+
+    public TaskModel getItem(int position) {
+        if (position >= 0 && position < taskModels.length) {
+            return taskModels[position];
+        }
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.task_list_item, parent, false);
+        return new ViewHolder(view, listItemListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        TaskModel taskModel = taskModels[position];
+        holder.title.setText(taskModel.getTitle());
+        holder.description.setText(taskModel.getDescription());
+        holder.points.setText(String.valueOf(taskModel.getPoints()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskModels.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView title;
+        final TextView description;
+        final TextView points;
+
+        public ViewHolder(@NonNull View itemView, ListItemListener listItemListener) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+            points = itemView.findViewById(R.id.storyPoints);
+            itemView.setOnClickListener(view -> {
+                int position = getAdapterPosition();
+                if (listItemListener != null && position != RecyclerView.NO_POSITION) {
+                    listItemListener.onItemClick(position);
+                }
+            });
+        }
+    }
+}
